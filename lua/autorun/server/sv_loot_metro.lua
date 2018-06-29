5de87153b1d0e7c0e6dc30e9fafeefa31988e096
Alex <popcorn797@gmail.com>
2018-06-29 11:57:44 -0500

    General improvements to performance and security pt2

--- lua/autorun/server/sv_loot_metro.lua
+++ lua/autorun/server/sv_loot_metro.lua
@@ -1,10 +1,5 @@
 util.AddNetworkString( "Net_requestLoot" )
-net.Receive( "Net_requestLoot", function( len, ply )
-	local NEnt = net.ReadEntity()
-	if ( CurTime() > NEnt:GetNWInt( "nextSearch" ) ) then
-		NEnt:Loot( ply )
-	end
-end)
-
+util.AddNetworkString("Loot_SetDrawProgress")
+util.AddNetworkString("Loot_EnableProgress")
 
 
